659b5f0d3e0a5059b30a1d8a9fcdf660650d3684
Pat Palmerston <patpalmerston@gmail.com>
2019-12-01 15:43:36 -0800

    first set of modules

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,12 +10,16 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+for x in sys.argv:
+    print("Argument",x)
 
+print("The name of the script:", sys.argv[0])
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print(sys.platform)
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print(sys.version_info)
 
 
 import os
